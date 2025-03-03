d9f90ce8c2c49794bb704091fd04acfcd6406a6a
Jonathan Gresl <jgresl@hotmail.com>
2025-03-02 23:47:29 -0700

    BONUS - Implement double jump and dash features

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,9 +5,14 @@ using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
+    public UnityEvent<Vector3> OnJump = new UnityEvent<Vector3>();
     private bool onGround = true;
-    public float jumpInput = 50f;
+    private int currentJump = 1;
+    private int maxJumps = 2;
 
+    // Move parameters visible to component
+    [SerializeField] private float jumpInput;
+    [SerializeField] private float speedInput;
     [SerializeField] private CinemachineCamera freeLookCamera;
 
     // Update is called once per frame
@@ -17,38 +22,61 @@ public class InputManager : MonoBehaviour
         Vector2 inputVector = Vector2.zero;
 
         // Get user input
-        if (Input.GetKey(KeyCode.W) && onGround)
+        if (Input.GetKey(KeyCode.W))
         {
             inputVector += Vector2.up;
         }
-        if (Input.GetKey(KeyCode.S) && onGround)
+        if (Input.GetKey(KeyCode.S))
         {
             inputVector += Vector2.down;
         }
-        if (Input.GetKey(KeyCode.D) && onGround)
+        if (Input.GetKey(KeyCode.D))
         {
             inputVector += Vector2.right;
         }
-        if (Input.GetKey(KeyCode.A) && onGround)
+        if (Input.GetKey(KeyCode.A))
         {
             inputVector += Vector2.left;
         }
 
-        float jump = 0f;
-        if (Input.GetKey(KeyCode.Space) && onGround)
-        {
-            jump = jumpInput;
-            onGround = false;
-        }
 
         // Create a 3D version of the 2D input vector
-        Vector3 inputXZPlane = new Vector3(inputVector.x, jump, inputVector.y);
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
 
         // Get the rotation angle from the free-look camera
         var cameraRotation = Quaternion.Euler(0, freeLookCamera.transform.rotation.eulerAngles.y, 0);
 
-        // Send the input data to the OnMove event
-        OnMove?.Invoke(cameraRotation * inputXZPlane);
+        if (onGround)
+        {
+            // Send the input data to the OnMove event
+            OnMove?.Invoke(cameraRotation * inputXZPlane * speedInput);
+        } else
+        {
+            // Reduce air control
+            OnMove?.Invoke(cameraRotation * inputXZPlane * speedInput * 0.5f);
+        }
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            // BONUS - Implement double jump feature
+            if (currentJump <= maxJumps)
+            {
+                currentJump++;
+                onGround = false;
+
+                // Create a 3D input vector for applying jump force
+                Vector3 inputYPlane = new Vector3(0, jumpInput, 0);
+
+                // Send the input data to the OnMove event
+                OnJump?.Invoke(cameraRotation * inputYPlane);
+
+                if (currentJump == 2) {
+                    // BONUS - Implment dash feature
+                    OnMove?.Invoke(cameraRotation * inputXZPlane * speedInput * 2.0f);
+                }
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -56,6 +84,7 @@ public class InputManager : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             onGround = true;
+            currentJump = 1;    
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,19 +2,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
-    [SerializeField] private float speed;
 
     private Rigidbody rb;
 
     private void Start()
     {
-        // Adding MovePlayer as a listener to the OnMove event
+        // Adding MovePlayer as a listener to the OnMove and OnJump events
         inputManager.OnMove.AddListener(MovePlayer);
+        inputManager.OnJump.AddListener(JumpPlayer);
         rb = GetComponent<Rigidbody>();
     }
 
     private void MovePlayer(Vector3 moveDirection)
     {
-        rb.AddForce(speed * moveDirection, ForceMode.Acceleration);
+        rb.AddForce(moveDirection, ForceMode.Force);
+    }
+
+    private void JumpPlayer(Vector3 jumpDirection)
+    {
+        Debug.Log("Jump Player");
+        rb.AddForce(jumpDirection, ForceMode.Impulse);
     }
 }
